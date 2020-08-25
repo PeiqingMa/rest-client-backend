dadb2f2c3309fd1ed55156ce70835e8916d97e1d
PeiqingMa <peiqing.ma@eventbank.com>
2020-08-25 11:29:57 +0800

    change to maven + java

--- src/native-test/kotlin/me/self/NativeExampleResourceIT.kt
+++
@@ -1,6 +0,0 @@
-package me.self
-
-import io.quarkus.test.junit.NativeImageTest
-
-@NativeImageTest
-class NativeExampleResourceIT : ExampleResourceTest()
---
+++ src/test/java/me/self/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package me.self;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
